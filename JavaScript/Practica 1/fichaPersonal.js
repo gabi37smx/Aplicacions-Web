1edb2e3efbf704562c8d1769dda4b196949c063d
GABI <gabrielvidal37@gmail.com>
2026-01-28 23:52:26 +0100

    Delete JavaScript directory

--- JavaScript/Practica 1/fichaPersonal.js
+++
@@ -1,5 +0,0 @@
-var nombre = prompt("¿Cuál es tu nombre?");
-var anyonac = Number(prompt("¿En qué año naciste?"));
-var anyos = 2025 - anyonac;
-
-alert("Hola " + nombre + ", tienes " + anyos + " años.");
